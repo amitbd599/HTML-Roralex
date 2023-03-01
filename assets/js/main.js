f66e66c6dc27484e857e25dbc50d36c24ab43a1e
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-03-01 22:16:16 +0600

    ourTeam-section-version-one responsive add

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,6 +186,32 @@
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        pagination: false,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+      },
+      500: {
+        slidesPerView: 1,
+        pagination: false,
+        spaceBetween: 20,
+        slidesPerGroup: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 4,
+      },
+    },
   });
 
   var TeamVersionTwo = new Swiper(".team-version-two", {
