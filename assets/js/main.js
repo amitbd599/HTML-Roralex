8ce2b056a794ae0f2e836d33ef3f206dbb213777
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2023-03-03 23:06:07 +0600

    Blog Section version Three responsive add

--- assets/js/main.js
+++ assets/js/main.js
@@ -513,6 +513,31 @@
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      500: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      992: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+      },
+      1200: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+      },
+      1400: {
+        slidesPerView: 3,
+        slidesPerGroup: 2,
+      },
+    },
   });
 
   // =========== Blog Slider End =========== Js //
